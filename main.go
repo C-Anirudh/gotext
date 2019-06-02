cac28430b790ce4c658b97e9c99cbdcff92ae690
C-Anirudh <canirudh16@gmail.com>
2019-06-02 23:15:11 +0530

    addded flag to count characters in file

--- main.go
+++ main.go
@@ -10,25 +10,67 @@ import (
 
 func main() {
 	wordFile := flag.String("w", "", "The name/path of text file to be parsed for count of words.")
+	charFile := flag.String("c", "", "The name/path of text file to be parsed for count of characters.")
 	flag.Parse()
 
-	if *wordFile == "" {
+	if *wordFile == "" && *charFile == "" {
 		printUsage()
 		os.Exit(1)
 	}
 
-	file, err := ioutil.ReadFile(*wordFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	r := string(file)
+	if *wordFile != "" && *charFile != "" {
+		if *wordFile == *charFile {
+			file, err := ioutil.ReadFile(*wordFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			r := string(file)
 
-	wc := wordCount(r)
-	fmt.Println(wc)
+			wc := wordCount(r)
+			cc := len(r)
+			fmt.Println(wc)
+			fmt.Println(cc)
+		} else {
+			filew, err := ioutil.ReadFile(*wordFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			filec, err := ioutil.ReadFile(*charFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			rw := string(filew)
+			rc := string(filec)
+
+			wc := wordCount(rw)
+			cc := len(rc)
+			fmt.Println(wc)
+			fmt.Println(cc)
+		}
+	} else if *wordFile != "" {
+		file, err := ioutil.ReadFile(*wordFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		r := string(file)
+
+		wc := wordCount(r)
+		fmt.Println(wc)
+	} else {
+		file, err := ioutil.ReadFile(*charFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		r := string(file)
+
+		cc := len(r)
+		fmt.Println(cc)
+	}
 }
 
 func printUsage() {
 	fmt.Printf("Usage: %s [options]\n", os.Args[0])
 	fmt.Println("Options:")
 	fmt.Println("\t -w\t The name/path of text file to be parsed for count of words.")
+	fmt.Println("\t -c\t The name/path of text file to be parsed for count of characters.")
 }
